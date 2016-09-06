ef2d1e5e07b4f7a0dc76cae4d3735b2083df1724
XZ <register.xizhou2@gmail.com>
2016-09-05 18:28:04 -0700

    Added dish and menu model.

--- backend/src/main/java/Models.java
+++ backend/src/main/java/Models.java
@@ -20,6 +20,7 @@ class User {
 
     private String firstName;
     private String lastName;
+    private long   mid;
 /*
     private final String phone;
     private final String address;
@@ -85,5 +86,174 @@ class User {
 
     public void setPassword(String pw) {
         password = pw;
+    }
+
+    public long getMid() {
+        return mid;
+    }
+
+    public void setMid(long id) {
+        mid = id;
+    }
+}
+
+@Entity
+@Table(name="menus")
+class Menu {
+    @Id
+    private long mid;
+    private String userName;
+    private String category;
+
+    public Menu() {}
+
+    public Menu(String user) {
+        this.userName = user;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String name) {
+        this.userName = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
+
+@Entity
+@Table(name="dishes")
+class Dish {
+    @Id
+    private long    did;
+    private String  userName;
+    private String  category;
+    private String  dishName;
+    private float   price;
+    private String  description;
+    private boolean Status;
+    private boolean monAvail;
+    private boolean tueAvail;
+    private boolean wedAvail;
+    private boolean thuAvail;
+    private boolean friAvail;
+    private boolean satAvail;
+    private boolean sunAvail;
+
+    // FIXME
+    // private char photo;
+
+    public Dish() {}
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String name) {
+        this.userName = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDishName() {
+        return dishName;
+    }
+
+    public void setCategory(String name) {
+        this.dishName = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean getMonAvail() {
+        return monAvail;
+    }
+
+    public void setMonAvail(boolean av) {
+        this.monAvail = av;
+    }
+
+    public boolean getTueAvail() {
+        return tueAvail;
+    }
+
+    public void setTueAvail(boolean av) {
+        this.tueAvail = av;
+    }
+
+    public boolean getWedAvail() {
+        return wedAvail;
+    }
+
+    public void setWedAvail(boolean wedAvail) {
+        this.wedAvail = wedAvail;
+    }
+
+    public boolean getThuAvail() {
+        return thuAvail;
+    }
+
+    public void setThuAvail(boolean thuAvail) {
+        this.thuAvail = thuAvail;
+    }
+
+    public boolean getFriAvail() {
+        return friAvail;
+    }
+
+    public void setCategory(boolean av) {
+        this.friAvail = av;
+    }
+
+    public boolean getSatAvail() {
+        return satAvail;
+    }
+
+    public void setSatAvail(boolean av) {
+        this.satStart = av;
+    }
+
+    public boolean getSunAvail() {
+        return sunAvail;
+    }
+
+    public void setSunAvail(boolean av) {
+        this.sunStart = av;
     }
 }
