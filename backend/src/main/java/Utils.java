0e74b03d7dbf7f387e1156ea4c2d12c64b81bc81
XZ <register.xizhou2@gmail.com>
2016-10-09 11:20:08 -0700

    Utils: removed validateUsername()

--- backend/src/main/java/Utils.java
+++ backend/src/main/java/Utils.java
@@ -9,17 +9,8 @@
 package forester.familykitchen;
 public class Utils {
     public static boolean validateString(String str) {
-        return str != null && str.length() > 0;
-    }
+        if(str == null && str.length() == 0) return false;
 
-    /**
-     * @brief: Validate Username 
-     */
-    public static boolean validateUsername(String name) {
-        if(!validateString(name)) {
-            return false;
-        }
-        
         return name.matches("^\\w[a-zA-Z0-9_@ ]*$");
     }
 }
